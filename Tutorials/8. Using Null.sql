7e98a6430ceb1505df45a8a8aad5e0e8a036b449
Christos Milarokostas <xristos_mil@hotmail.com>
2017-10-07 20:51:33 +0000

    Update 8. Using Null.sql

--- Tutorials/8. Using Null.sql
+++ Tutorials/8. Using Null.sql
@@ -1,65 +1,65 @@
-#Using Null
-#===========
+-- Using Null
+-- ===========
 
 
-#1. List the teachers who have NULL for their department.
-#--------------------------------------------------------
+-- 1. List the teachers who have NULL for their department.
+-- --------------------------------------------------------
 SELECT name
 FROM teacher
 WHERE dept IS NULL;
 
 
-#2. Note the INNER JOIN misses the teachers with no department and the departments with no teacher.
-#--------------------------------------------------------------------------------------------------
+-- 2. Note the INNER JOIN misses the teachers with no department and the departments with no teacher.
+-- --------------------------------------------------------------------------------------------------
 SELECT t.name, d.name
 FROM teacher t, dept d
 WHERE t.dept = d.id;
 
 
-#3. Use a different JOIN so that all teachers are listed.
-#--------------------------------------------------------
+-- 3. Use a different JOIN so that all teachers are listed.
+-- --------------------------------------------------------
 SELECT t.name, d.name
 FROM teacher t LEFT JOIN dept d ON t.dept = d.id;
 
 
-#4. Use a different JOIN so that all departments are listed.
-#-----------------------------------------------------------
+-- 4. Use a different JOIN so that all departments are listed.
+-- -----------------------------------------------------------
 SELECT t.name, d.name
 FROM teacher t RIGHT JOIN dept d ON t.dept = d.id;
 
 
-#5. Use COALESCE to print the mobile number. Use the number '07986 444 2266' if there is no number given. Show teacher name and mobile number or '07986 444 2266'.
-#-----------------------------------------------------------------------------------------------------------------------------------------------------------------
+-- 5. Use COALESCE to print the mobile number. Use the number '07986 444 2266' if there is no number given. Show teacher name and mobile number or '07986 444 2266'.
+-- -----------------------------------------------------------------------------------------------------------------------------------------------------------------
 SELECT name, COALESCE(mobile, '07986 444 2266') AS 'mobile number'
 FROM teacher;
 
 
-#6. Use the COALESCE function and a LEFT JOIN to print the teacher name and department name. Use the string 'None' where there is no department.
-#-----------------------------------------------------------------------------------------------------------------------------------------------
+-- 6. Use the COALESCE function and a LEFT JOIN to print the teacher name and department name. Use the string 'None' where there is no department.
+-- -----------------------------------------------------------------------------------------------------------------------------------------------
 SELECT t.name, COALESCE(d.name, 'None')
 FROM teacher t LEFT JOIN dept d ON t.dept = d.id;
 
 
-#7. Use COUNT to show the number of teachers and the number of mobile phones.
-#----------------------------------------------------------------------------
+-- 7. Use COUNT to show the number of teachers and the number of mobile phones.
+-- ----------------------------------------------------------------------------
 SELECT COUNT(id), COUNT(mobile)
 FROM teacher;
 
 
-#8. Use COUNT and GROUP BY dept.name to show each department and the number of staff. Use a RIGHT JOIN to ensure that the Engineering department is listed.
-#----------------------------------------------------------------------------------------------------------------------------------------------------------
+-- 8. Use COUNT and GROUP BY dept.name to show each department and the number of staff. Use a RIGHT JOIN to ensure that the Engineering department is listed.
+-- ----------------------------------------------------------------------------------------------------------------------------------------------------------
 SELECT d.name, COUNT(t.name)
 FROM teacher t RIGHT JOIN dept d ON t.dept = d.id
 GROUP BY d.name;
 
 
-#9. Use CASE to show the name of each teacher followed by 'Sci' if the teacher is in dept 1 or 2 and 'Art' otherwise.
-#--------------------------------------------------------------------------------------------------------------------
+-- 9. Use CASE to show the name of each teacher followed by 'Sci' if the teacher is in dept 1 or 2 and 'Art' otherwise.
+-- --------------------------------------------------------------------------------------------------------------------
 SELECT name, CASE WHEN dept = 1 OR dept = 2 THEN 'Sci' ELSE 'Art' END
 FROM teacher;
 
 
-#10. Use CASE to show the name of each teacher followed by 'Sci' if the teacher is in dept 1 or 2, show 'Art' if the teacher's dept is 3 and 'None' otherwise.
-#-------------------------------------------------------------------------------------------------------------------------------------------------------------
+-- 10. Use CASE to show the name of each teacher followed by 'Sci' if the teacher is in dept 1 or 2, show 'Art' if the teacher's dept is 3 and 'None' otherwise.
+-- -------------------------------------------------------------------------------------------------------------------------------------------------------------
 SELECT name, CASE WHEN dept = 1 OR dept = 2 THEN 'Sci' WHEN dept = 3 THEN 'Art' ELSE 'None' END
 FROM teacher;
