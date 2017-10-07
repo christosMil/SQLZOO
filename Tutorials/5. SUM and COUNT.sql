fb310fdef3df30a69575accaefcb242fda09a224
Christos Milarokostas <xristos_mil@hotmail.com>
2017-10-07 20:43:13 +0000

    Update 5. SUM and COUNT.sql

--- Tutorials/5. SUM and COUNT.sql
+++ Tutorials/5. SUM and COUNT.sql
@@ -1,57 +1,57 @@
-#SUM and COUNT
-#=============
+-- SUM and COUNT
+-- =============
 
 
-#1. Total world population
-#-------------------------
+-- 1. Total world population
+-- -------------------------
 SELECT SUM(population)
 FROM world;
 
 
-#2. List of continents
-#---------------------
+-- 2. List of continents
+-- ---------------------
 SELECT DISTINCT continent
 FROM world;
 
 
-#3. GDP of Africa
-#----------------
+-- 3. GDP of Africa
+-- ----------------
 SELECT SUM(gdp)
 FROM world
 WHERE continent = 'Africa';
 
 
-#4. Count the big countries
-#--------------------------
+-- 4. Count the big countries
+-- --------------------------
 SELECT COUNT(name)
 FROM world
 WHERE area >= 1000000;
 
 
-#5. Baltic states population
-#---------------------------
+-- 5. Baltic states population
+-- ---------------------------
 SELECT SUM(population)
 FROM world
 WHERE name IN ('Estonia', 'Latvia', 'Lithuania');
 
 
-#6. Counting the countries of each continent
-#-------------------------------------------
+-- 6. Counting the countries of each continent
+-- -------------------------------------------
 SELECT continent, COUNT(name)
 FROM world
 GROUP BY continent;
 
 
-#7. Counting big countries in each continent
-#-------------------------------------------
+-- 7. Counting big countries in each continent
+-- -------------------------------------------
 SELECT continent, COUNT(name)
 FROM world
 WHERE population >= 10000000
 GROUP BY continent;
 
 
-#8. Counting big continents
-#--------------------------
+-- 8. Counting big continents
+-- --------------------------
 SELECT continent
 FROM world
 GROUP BY continent
